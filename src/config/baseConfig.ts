df477333b0ff03ebe456c170c51c4fe90cb30a6f
Eugene Petrik <eugeniy.petrik@gmail.com>
2025-04-08 21:37:25 +0300

    Add envs config

---
+++ src/config/baseConfig.ts
@@ -0,0 +1,18 @@
+import { config as dotenvConfig } from 'dotenv';
+import { join } from 'path';
+import { IBaseConfig } from '../../typings/global';
+
+dotenvConfig({
+  path: join(process.cwd(), '.env'),
+});
+
+export const baseConfig: IBaseConfig = {
+  WEB_URL: process.env.WEB_URL! ?? 'https://practicesoftwaretesting.com',
+  API_URL: process.env.API_URL!,
+  USER_EMAIL: process.env.USER_EMAIL!,
+  USER_PASSWORD: process.env.USER_PASSWORD!,
+  USER_NAME: process.env.USER_NAME!,
+  ADMIN_EMAIL: process.env.ADMIN_EMAIL!,
+  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD!,
+  ADMIN_NAME: process.env.ADMIN_NAME!,
+};
---
+++ typings/global.d.ts
@@ -0,0 +1,10 @@
+export interface IBaseConfig {
+  WEB_URL: string;
+  API_URL: string;
+  USER_EMAIL: string;
+  USER_PASSWORD: string;
+  USER_NAME: string;
+  ADMIN_EMAIL: string;
+  ADMIN_PASSWORD: string;
+  ADMIN_NAME: string;
+}
