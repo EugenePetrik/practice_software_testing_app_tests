76acc4edf4bfcc9aec2ee0b4cc01e08a0ea2a518
Eugene Petrik <eugeniy.petrik@gmail.com>
2025-04-12 12:16:07 +0300

    Remove WEB_URL from config

--- src/config/baseConfig.ts
+++ src/config/baseConfig.ts
@@ -7,7 +7,7 @@ dotenvConfig({
 });
 
 export const baseConfig: IBaseConfig = {
-  WEB_URL: process.env.WEB_URL! ?? 'https://practicesoftwaretesting.com',
+  WEB_URL: process.env.WEB_URL!,
   API_URL: process.env.API_URL!,
   USER_EMAIL: process.env.USER_EMAIL!,
   USER_PASSWORD: process.env.USER_PASSWORD!,
