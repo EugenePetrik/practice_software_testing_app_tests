930ce78c0a443fec457d275a5410250364826735
Eugene Petrik <eugeniy.petrik@gmail.com>
2025-04-12 12:06:38 +0300

    Add tests for sorting

---
+++ testData/sortOptions.ts
@@ -0,0 +1,16 @@
+import { SortOption } from '../src/pages/home/fragment/filter.fragment';
+
+export const dataSortType: { sortBy: SortOption }[] = [
+  {
+    sortBy: 'Name (A - Z)',
+  },
+  {
+    sortBy: 'Name (Z - A)',
+  },
+  {
+    sortBy: 'Price (Low - High)',
+  },
+  {
+    sortBy: 'Price (High - Low)',
+  },
+];
