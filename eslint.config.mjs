68c5d424ef6f1fcf269c2e437635d1541577f04c
Eugene Petrik <eugeniy.petrik@gmail.com>
2025-04-14 09:55:39 +0300

    Update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import googleConfig from 'eslint-config-google';
 import stylistic from '@stylistic/eslint-plugin';
 import playwright from 'eslint-plugin-playwright';
 import checkFile from 'eslint-plugin-check-file';
@@ -8,14 +7,24 @@ import checkFile from 'eslint-plugin-check-file';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylistic,
-  googleConfig,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['eslint.config.mjs'],
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
   {
     files: ['**/*.{js,mjs,cjs,ts}'],
     plugins: {
       '@stylistic': stylistic,
-      playwright,
       'check-file': checkFile,
     },
     rules: {
@@ -25,18 +34,41 @@ export default [
       'max-len': 'off', // deprecated
       'operator-linebreak': ['error', 'before'],
       'no-empty-pattern': 'off',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/await-thenable': 'error',
       '@stylistic/object-curly-spacing': ['error', 'always'],
-      '@stylistic/max-len': ['error', { 'code': 150, 'ignorePattern': '^import\\s.+\\sfrom\\s.+;$' }],
-      '@stylistic/type-annotation-spacing': ['error', { 'before': false, 'after': true }],
-      '@stylistic/space-infix-ops': ['error', { 'int32Hint': false }],
+      '@stylistic/max-len': [
+        'error',
+        { code: 150, comments: 9999, ignoreStrings: true },
+      ],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/spaced-comment': ['error', 'always'],
+      '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/no-multi-spaces': ['error'],
+      '@stylistic/type-annotation-spacing': [
+        'error',
+        { 'before': false, 'after': true },
+      ],
+      '@stylistic/space-infix-ops': [
+        'error',
+        { int32Hint: false },
+      ],
+      'no-undef': 'off',
+      'no-unused-vars': 'error',
+      'space-before-blocks': 'error',
       '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
     },
   },
   // Playwright ESlint config
   {
+    ...playwright.configs['flat/recommended'],
     files: ['src/tests/**'],
     rules: {
+      ...playwright.configs['flat/recommended'].rules,
       'playwright/no-focused-test': 'error',
+      'playwright/expect-expect': 'off',
     },
   },
   // Check file ESlint config
@@ -54,10 +86,12 @@ export default [
   },
   {
     ignores: [
+      'eslint.config.mjs',
       'node_modules',
       'playwright-report',
-      'test-report',
       'test-results',
+      'blob-report',
+      'html-report',
     ],
   },
 ];
