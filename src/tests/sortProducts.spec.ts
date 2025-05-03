a1b7990108df81d2287e249bb295c81bc20c073f
Eugene Petrik <eugeniy.petrik@gmail.com>
2025-05-03 11:39:55 +0300

    Login via API + fixture

--- src/tests/sortProducts.spec.ts
+++ src/tests/sortProducts.spec.ts
@@ -2,9 +2,13 @@ import { test } from '../fixtures';
 import { dataSortType } from '../../testData/sortOptionsData';
 
 dataSortType.forEach(({ sortBy }) => {
-  test(`Verify user can perform sorting products by "${sortBy}"`, async ({ app }) => {
+  test(`Verify user can perform sorting products by "${sortBy}"`, async ({ loggedNewUserApp: { app, newUser } }) => {
     await test.step('Navigate to the home page', async () => {
       await app.home.open();
+
+      console.log(newUser.email);
+      console.log(newUser.first_name);
+      console.log(newUser.last_name);
     });
 
     await test.step('Select sorting option', async () => {
